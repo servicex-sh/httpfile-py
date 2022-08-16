facbaa3e31c7080616dec46da8842c5bac72e397
linux_china <libing.chen@gmail.com>
2022-08-15 22:12:57 -0700

    chore: add typings

--- httpfile-async-test.py
+++ httpfile-async-test.py
@@ -2,10 +2,11 @@ import httpfile.loader
 # noinspection PyUnresolvedReferences
 import index
 import asyncio
+from httpx import Response
 
 
 async def my_ip():
-    r = await index.async_my_ip()
+    r: Response = await index.async_my_ip()
     print(r.json())
 
 
--- httpfile-test.py
+++ httpfile-test.py
@@ -1,20 +1,21 @@
 import httpfile.loader
 # noinspection PyUnresolvedReferences
 import index
+from httpx import Response
 
 
 def my_ip():
-    r = index.my_ip()
+    r: Response = index.my_ip()
     print(r.json())
 
 
 def post_test():
-    r = index.post_test(nick="linux_china", host="httpbin.org")
+    r: Response = index.post_test(nick="linux_china", host="httpbin.org")
     print(r.json())
 
 
 def graphql_test():
-    r = index.graphql_demo(nick="linux_china")
+    r: Response = index.graphql_demo(nick="linux_china")
     print(r.json())
 
 
